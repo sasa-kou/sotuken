73d590776b799ae7af7dac010e55becc0f7ba36f
sasa-kou <sasakou11428@gmail.com>
2019-11-20 14:01:12 +0900

    add label

--- pause.py
+++ pause.py
@@ -16,8 +16,9 @@ testData_num = ['1', '2', '3', '4', '5', '6', '7']
 class Outou:
     def __init__(self, file_name):
         self.outou = []
-        self.outou_label = {}
+        self.outou_label = []
         self.outou_compare = {}
+        self.outou_label_compare = {}
         self.file_name = file_name
 
     def read_data(self):  # 全ファイルを検索して読み込み
@@ -35,9 +36,11 @@ class Outou:
 
                 num = file_index + '-' + index
                 self.outou_compare.update({num: self.outou})
+                self.outou_label_compare.update({num: self.outou_label})
                 self.outou = []
+                self.outou_label = []
 
-        return self.outou_label, self.outou_compare
+        return self.outou_compare, self.outou_label_compare
 
     def readOutou(self, file_name):  # 語り手側  {語り終了時間:言葉}
         file = open(file_name)  # データ入力
@@ -77,7 +80,7 @@ class Outou:
                 # print(word, label, begin)
 
                 self.outou.append({begin: word})
-                self.outou_label.update({begin: label})
+                self.outou_label.append({begin: label})
 
             else:
                 tmp_str += data[0]  # 文字の退避
@@ -172,9 +175,13 @@ class Pause:
 
 
 def trance_data(pause, outou):
-    interval = 0
-    length = []  # 間の長さが200ms以上なら1
+    interval = 0    # すべての間での応答数
+    overThreshold = []  # 間の長さが200ms以上なら1
+    intervalData = {}
+    overThresholdData = {}
     for index in list(pause.keys()):
+        intervalData[index] = []
+        overThresholdData[index] = []
         for time in pause[index]:
             start = float(time[0])
             end = float(time[1])
@@ -183,16 +190,49 @@ def trance_data(pause, outou):
                 time = list(data.keys())[0]
                 if start < time and time < end:
                     interval += 1
+                    intervalData[index].append(data)
                     if round(end-start, 2) >= 0.2:
-                        length.append(1)
+                        overThreshold.append(1)
+                        overThresholdData[index].append(data)
                     else:
-                        length.append(0)
-
+                        overThreshold.append(0)
                     # print(start, end)
                     # print(time, end=' ')
                     # print(outou[index][time])
 
-    return interval, length
+    return interval, overThreshold, intervalData, overThresholdData
+
+
+def readLabelData(file_name):   # 現状使う予定なし
+    path = 'toukeiLabel' + file_name + '.txt'
+    data = []
+    with open(path, mode='r') as f:
+        data = f.read()
+    data = data.split('\n')[:-1]
+    return data
+
+
+def labelConversion(intervalData, outouLabelData):
+    labelList = []  # 出現するラベルをすべて保存
+    keyLabel = []   # ラベルの種類を保存
+    tmp = {}
+    result = []
+    for index in list(outouLabelData.keys()):
+        for data in intervalData[index]:
+            time = list(data.keys())[0]
+            value = list(filter(lambda x: time == list(
+                x.keys())[0], outouLabelData[index]))[0]
+            label = list(value.values())[0]
+            labelList.append(label)
+            if label not in keyLabel:
+                keyLabel.append(label)
+
+    for label in keyLabel:
+        tmp.update({label: labelList.count(label)})
+
+    for k, v in sorted(tmp.items(), key=lambda x: -x[1]):
+        result.append({k: v})
+    print(result)
 
 
 if __name__ == '__main__':
@@ -204,34 +244,45 @@ if __name__ == '__main__':
 
     print('A')
     ot = Outou('a')
-    outou_a_label, outou_a = ot.read_data()
+    outou_a, outou_a_label = ot.read_data()
     num = ot.count()
     print('応答数：', num)
-    interval, length = trance_data(pause, outou_a)
+    interval, overThreshold, intervalData, overThresholdData = trance_data(
+        pause, outou_a)
     print('間での応答数：', interval)
-    print('そのうち200ms以上の語りの間での応答数：', length.count(1))
-    print('200ms以上の語りの間における応答割合：', round(length.count(1)/pause_orver*100, 2))
-    print('全応答のうち200ms以上の間での応答割合：', round(length.count(1)/num*100, 2))
+    print('そのうち200ms以上の語りの間での応答数：', overThreshold.count(1))
+    labelConversion(overThresholdData, outou_a_label)
+    print('200ms以上の語りの間における応答割合：', round(
+        overThreshold.count(1)/pause_orver*100, 2), overThreshold.count(1), '/', pause_orver)
+    print('全応答のうち200ms以上の間での応答割合：', round(overThreshold.count(
+        1)/num*100, 2), overThreshold.count(1), '/', num)
 
     print('B')
     ot = Outou('b')
-    outou_b_label, outou_b = ot.read_data()
+    outou_b, outou_b_label = ot.read_data()
     num = ot.count()
     print('応答数：', num)
-    interval, length = trance_data(pause, outou_b)
+    interval, overThreshold, intervalData, overThresholdData = trance_data(
+        pause, outou_b)
     print('間での応答数：', interval)
-    print('そのうち200ms以上の語りの間での応答数：', length.count(1))
-    print('200ms以上の語りの間における応答割合：', round(length.count(1)/pause_orver*100, 2))
-    print('全応答のうち200ms以上の間での応答割合：', round(length.count(1)/num*100, 2))
+    print('そのうち200ms以上の語りの間での応答数：', overThreshold.count(1))
+    labelConversion(overThresholdData, outou_b_label)
+    print('200ms以上の語りの間における応答割合：', round(
+        overThreshold.count(1)/pause_orver*100, 2), overThreshold.count(1), '/', pause_orver)
+    print('全応答のうち200ms以上の間での応答割合：', round(overThreshold.count(
+        1)/num*100, 2), overThreshold.count(1), '/', num)
 
     print('C')
     ot = Outou('c')
-    outou_c_label, outou_c = ot.read_data()
+    outou_c, outou_c_label = ot.read_data()
     num = ot.count()
     print('応答数：', num)
-    interval, length = trance_data(pause, outou_c)
+    interval, overThreshold, intervalData, overThresholdData = trance_data(
+        pause, outou_c)
     print('間での応答数：', interval)
-    print('そのうち200ms以上の語りの間での応答数：', length.count(1))
-    print('200ms以上の語りの間における応答割合：', round(length.count(1)/pause_orver*100, 2))
-    print('全応答のうち200ms以上の間での応答割合：', round(length.count(1)/num*100, 2))
-    
+    print('そのうち200ms以上の語りの間での応答数：', overThreshold.count(1))
+    labelConversion(overThresholdData, outou_c_label)
+    print('200ms以上の語りの間における応答割合：', round(
+        overThreshold.count(1)/pause_orver*100, 2), overThreshold.count(1), '/', pause_orver)
+    print('全応答のうち200ms以上の間での応答割合：', round(overThreshold.count(
+        1)/num*100, 2), overThreshold.count(1), '/', num)
